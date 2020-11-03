ea4232c03b2f81628f752b7c88b6ba66f361d392
keita080315 <72794374+keita080315@users.noreply.github.com>
2020-11-03 17:00:35 +0900

    Add files via upload

---
+++ index.html/js/script.js
@@ -0,0 +1,33 @@
+$(function(){
+  $('.btn').click(function(){
+    var href=$(this).attr("href");
+    switch(href)
+    {
+      case '#1':
+      var target=$('#btn1');
+      break;
+      case '#2':
+      var target=$('#btn2');
+      break;
+      case '#3':
+      var target=$('#btn3');
+      break;
+      case '#4':
+      var target=$('#btn4');
+      break;
+
+    }
+
+    var position=target.offset();
+    $('html,body').animate({'scrollTop':(position.top)},'slow');
+  });
+  $('.top').bgSwitcher({
+    images:['fv-bgi_01@2x.jpg','fv-bgi_02@2x.jpg','fv-bgi_03@2x.jpg'],
+    interval:8000,
+    effect:"fade",
+    duration:500,
+  });
+
+
+
+});
